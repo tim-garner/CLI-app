f6a5a1fa61146a577405f00fd142b4c14ce14736
tim-garner <timothy.garner602@gmail.com>
2019-06-05 22:53:35 -0400

    kinda works with JS but still not connecting SQL product name

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,5 +1,8 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+// var choices= ["PS4", "Apple Watch","Callaway Epic Flash Driver", "Rolex Oyster","The Sandlot","12 pack Pepsi","BioShock", "Brooks Running Shoes", "Steph Curry Jersey","1965 Ford Shelby", "EXIT"];
+// var stock= [15,35,10,3,40,80,5,25,45,1];
+
 
 
 var connection = mysql.createConnection({
@@ -27,29 +30,49 @@ connection.connect(function(err) {
   });
 // });
 
+
 function start() {
     inquirer
-      .prompt({
+      .prompt([{
         name: "Introduction!",
         type: "list",
         message: "What would you like to buy??",
         choices: ["PS4", "Apple Watch","Callaway Epic Flash Driver", "Rolex Oyster","The Sandlot","12 pack Pepsi","BioShock", "Brooks Running Shoes", "Steph Curry Jersey","1965 Ford Shelby", "EXIT"]
-      })
-    .then(function(answer) {
-        if (answer.Introduction === "PS4", "Apple Watch","Callaway Epic Flash Driver", "Rolex Oyster","The Sandlot","12 pack Pepsi","BioShock", "Brooks Running Shoes", "Steph Curry Jersey","1965 Ford Shelby") {
+      },
+      { type: "input",
+      name: "inputAmount",
+      message: "How many units would you like?",
+    }
+  ])
+.then(function(start) {
+        if (start.Introduction === "PS4", "Apple Watch","Callaway Epic Flash Driver", "Rolex Oyster","The Sandlot","12 pack Pepsi","BioShock", "Brooks Running Shoes", "Steph Curry Jersey","1965 Ford Shelby") {
          console.log("Great Option");
           Selection();
     
         }
-        else(answer.Introduction === "EXIT") 
+        else(start.Introduction === "EXIT") 
           console.log("leaving store");
           connection.end();
-        });
-      }
+      });
+    }
+
+
+    
 
       function Selection() {
         connection.query("SELECT * FROM products", function(err, results) {
           if (err) throw err;
-         console.log("working on solution");
-        });
+          console.log("working");
+          for (var i = 0; i <product_name; i++) {
+            if (start.inputNumber > stock_quantity) {
+              console.log("out of stock");
+              start();
+            } else{
+              console.log("order is now complete! Have a nice day!")
+            }
+          }
+       
+        }
+        );
       }
+      //change choices.length and choice.stock
